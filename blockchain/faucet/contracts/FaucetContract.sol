2ae15cd900d1523445146bd9769a0b60cd2b5f23
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-07-19 17:14:57 +0300

    change compiler version and create function to recieve value(ETH)

--- blockchain/faucet/contracts/FaucetContract.sol
+++ blockchain/faucet/contracts/FaucetContract.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.16 <0.9.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 	// storage variables (data,state that can persist over time)
-	uint public funds = 1000;
-	int public counter = -10;
-	uint32 public test = 4294967295; // 2 on 32
+	receive() external payable {}
 
 }
