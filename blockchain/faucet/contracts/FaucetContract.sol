1eba24c97f8076d685059c01bd004fb60020bda9
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-07-20 14:35:08 +0300

    add function to add funds in the Faucet smart contract

--- blockchain/faucet/contracts/FaucetContract.sol
+++ blockchain/faucet/contracts/FaucetContract.sol
@@ -1,8 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+
 contract Faucet {
-	// storage variables (data,state that can persist over time)
+	// special function
+	// it's called when you make a transaction (tx) that doesn't
+	// specify function name to call
+
+	//External functions are part of the Contract interface
+	//which means they can be called via contracts and other
+	// transactions 
+
 	receive() external payable {}
+
+	function addFunds()  external payable{
+
+	}
 
 }
--- blockchain/faucet/migrations/2_faucet_migration.js
+++ blockchain/faucet/migrations/2_faucet_migration.js
@@ -3,3 +3,8 @@ const FaucetContract = artifacts.require('Faucet');
 module.exports = function (deployer) {
   deployer.deploy(FaucetContract);
 };
+
+// 0x fdacd576 0000000000000000000000000000000000000000000000000000000000000002
+// 0x = prefix to say that the number is hexadecimal
+// fdac576 = call the setCompleted function from the Migrations.sol (the 1ts 4 bytes is the function signature hashed)
+// 00000.. 2 = 32bytes(64 chars) ending with 2 = the argument to pass to the function
